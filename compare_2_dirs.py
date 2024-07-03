2452f66422b11fa3bc1c5282613b8c9221af25df
Biraj Shrestha <biraj@CMI-RSCH-LT117.childmind.org>
2024-07-03 18:17:15 -0400

    filename and file contents test separated

--- compare_2_dirs.py
+++ compare_2_dirs.py
@@ -1,6 +1,5 @@
 import unittest
 import os
-import filecmp
 
 DIR1 = "/home/biraj/bids_parsing/outputs/working/pipeline_cpac_anat/anna_raw/sub-PA001/ses-V1W1"
 DIR2 = "/home/biraj/bids_parsing/dataset/anna/raw/sub-PA001/ses-V1W1"
@@ -17,32 +16,27 @@ class DirectoryComparisonTest(unittest.TestCase):
         with open(file1, 'rb') as f1, open(file2, 'rb') as f2:
             return f1.read() == f2.read()
 
-    def test_compare_directories(self):
-        dir1 = DIR1
-        dir2 = DIR2
+    def test_matching_file_names(self):
+        """Test if both directories have the same set of files."""
+        files1 = list(self.list_files(DIR1))
+        files2 = list(self.list_files(DIR2))
 
-        files1 = list(self.list_files(dir1))
-        files2 = list(self.list_files(dir2))
+        rel_files1 = {os.path.relpath(f, DIR1) for f in files1}
+        rel_files2 = {os.path.relpath(f, DIR2) for f in files2}
 
-        # Convert file paths to relative paths for comparison
-        rel_files1 = {os.path.relpath(f, dir1) for f in files1}
-        rel_files2 = {os.path.relpath(f, dir2) for f in files2}
-
-        # Check if both directories have the same set of files
         self.assertSetEqual(rel_files1, rel_files2, "Directories do not have the same files")
 
-        matched_files_count = 0
-        # For each file in directory 1, compare its contents with the corresponding file in directory 2
-        for file_rel_path in rel_files1:
-            file1 = os.path.join(dir1, file_rel_path)
-            file2 = os.path.join(dir2, file_rel_path)
-            if self.compare_file_contents(file1, file2):
-                matched_files_count += 1
-            else:
-                print(f"File contents do not match for {file_rel_path}")
+    def test_matching_file_contents(self):
+        """Test if the contents of matching files are the same."""
+        files1 = list(self.list_files(DIR1))
+        rel_files1 = {os.path.relpath(f, DIR1) for f in files1}
 
-        print(f"Number of matched files: {matched_files_count}")
-        self.assertEqual(len(rel_files1), matched_files_count, "Not all files matched in content")
+        for file_rel_path in rel_files1:
+            file1 = os.path.join(DIR1, file_rel_path)
+            file2 = os.path.join(DIR2, file_rel_path)
+
+            with self.subTest(file=file_rel_path):
+                self.assertTrue(self.compare_file_contents(file1, file2), f"File contents do not match for {file_rel_path}")
 
 if __name__ == '__main__':
     unittest.main()
